0df6e3598fcccd69e1f4dec12dc287f286edc623
Donald <b2academo.donald@gmail.com>
2023-03-24 11:40:29 +0400

    remove unused files, add readme and update package.json file

--- src/handler.js
+++ src/handler.js
@@ -1,26 +1,37 @@
-/**
- * Compare two objects
- * @param {object} objA - The first object to compare
- * @param {object} objB - The second object to compare
- * @returns {string} 
- */
-function compareObjects(objA, objB) {
-  let changedCode;
-  for (const key in objA) {
-    if (objA.hasOwnProperty(key) && objB.hasOwnProperty(key)) {
-      if (objA[key] !== objB[key]) {
-        changedCode = key;
+import  _ from 'lodash';
+import crypto from 'crypto';
+
+const salt = 'Hashing salt string';
+
+function compareObjects(obj1, obj2) {
+  const diff = {};
+  const keys = Object.keys(obj1).concat(Object.keys(obj2));
+  
+  keys.forEach(key => {
+    if (obj1[key] !== obj2[key]) {
+      if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
+        diff[key] = compareObjects(obj1[key], obj2[key]);
+      } else {
+        diff[key] = [obj1[key], obj2[key]];
       }
     }
-  }
-  return changedCode;
+  });
+  
+  return diff;
 }
 
-/**
- * Fetch all the invites of an Guild and their respectives usage values
- * @param {object} guild - the discord server on which we are working
- * @returns {object}
- */
+const findDifference = (obj) => {
+  let result = null;
+  for (let item in obj) {
+    const [a, b] = obj[item];
+    if (Math.abs(a - b) === 1) {
+      result = { [item]: [a, b] };
+      break;
+    }
+  }
+  return result;
+}
+
 async function fetchGuildInvites (guild) {
   const inviteCounts = {};
 
@@ -30,41 +41,80 @@ async function fetchGuildInvites (guild) {
         inviteCounts[invite.code] = invite.uses;
       });      
     })
-
   return inviteCounts;
 }
 
-/**
- * Get new invite values and check the one which has incremented.
- * @param {object} member - The new member object 
- * @returns  {object} Information of the new user.
- */
 async function onNewUser(member, client) {
   if(member.user.bot) return;
   
   const guild = member.guild;
 
-  // Get the new guild invite and compare them with their previous values.
-  const newInvites = await fetchGuildInvites(guild)
+  const newInvites = await fetchGuildInvites(guild);
 
-  const usedCode = compareObjects(client.guildInvites, newInvites);
+  const inviteUsed = compareObjects(client.guildInvites, newInvites);
 
-  if (usedCode && usedCode.length > 0) {
-  const serverName = guild.name;
-  const message = `${member.user.username} with the ID: ${member.user.id} joined using code ${usedCode} on server ${serverName}`;
-  console.log(message);
-  
+  const usedCode = Object.keys(inviteUsed).length === 1 ?
+     Object.keys(inviteUsed)[0] :
+     findDifference(inviteUsed);  
+
+  if (!_.isEmpty(usedCode)) {
+    const serverName = guild.name;
+    const message = `${member.user.username} with the ID: ${member.user.id} joined using code ${usedCode} on server ${serverName}`;
+    console.log(message);
+
     return {
-      name: `${member.user.username}`,
-      code: `${usedCode}`,
-      userId: `${member.user.id}`
+      userName: `${member.user.username}`,
+      userCode: `${usedCode}`,
+      userId: `${member.user.id}`,
+      joinedAt: Math.floor(Date.now() / 1000)
     };
   }
 }
 
+const generateHash = (str, salt) => {
+  if (!_.isEmpty(str)) {
+    return crypto.createHmac('sha256', salt)
+      .update(str)
+      .digest('hex');
+  }
+  return;
+};
+
+const saveUser = async (db, userId, userCode, userName) => {
+  await db.query(`
+  INSERT INTO users (userId, userCode, userName) VALUES (?, ?, ?)
+`, [userId, userCode, userName]);
+};
+
+function getEventData(user) {
+  let params;
+  params =  {'data': [{
+    'event_name': 'CompleteRegistration',
+    'event_time': Math.floor(Date.now() / 1000),
+    'event_source_url': 'https://www.0xbattleground.com',
+    'action_source': 'website',
+    'user_data': {
+      'fn': generateHash(user.userName, salt) || ' ',
+      'external_id': generateHash(user.userId, salt) || '',
+      'fbc': `${process.env.FBC}`,
+      'fbp': `${process.env.FBP}`,
+      'subscription_id': user.code,
+      'client_user_agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.5 Mobile/15E148 Safari/604.1'
+    },
+    'custom_data': {
+      'content_name': 'Discord Server Join',
+      'currency': user.code,
+      'status': true,
+      'value': 0
+    },
+  }]}
+  console.log('params: ', params);
+  return params;
+}
 
 export default {
   compareObjects,
   fetchGuildInvites,
-  onNewUser
+  onNewUser,
+  getEventData
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import { Client, GatewayIntentBits } from 'discord.js';
+import mysql from 'mysql';
 import handler from './handler.js';
 import sendEvent from './sendEvent.js';
 
@@ -13,30 +14,97 @@ const client = new Client({ intents: [
   ]
 });
 
+const data = {
+  host: `${process.env.MYSQL_HOST}`,
+  user: `${process.env.MYSQL_USER}`,
+  password: `${process.env.MYSQL_PASSWORD}`,
+  database: `${process.env.MYSQL_DATABASE}`,
+}
+const table = process.env.MYSQL_TABLE_NAME;
+
+const db = mysql.createConnection(data);
+
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to MySQL database!');
+  
+  const schemaQuery = `CREATE SCHEMA IF NOT EXISTS ${data.database}`;
+  db.query(schemaQuery, (err, result) => {
+    if (err) throw err;
+  });
+  
+  const tableQuery =`
+  CREATE TABLE IF NOT EXISTS ${table} (
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    userId VARCHAR(255) NOT NULL,
+    userCode VARCHAR(255) NOT NULL,
+    userName VARCHAR(255) NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  )
+  `
+  db.query(tableQuery, (err, result) => {
+    if (err) throw err;
+    console.log('Table created or exists!');
+  });
+
+});
+
+
 client.once('ready', async () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
-  const guild = client.guilds.cache.get(process.env.GUILD_ID);
-
-  client.guildInvites = await handler.fetchGuildInvites(guild);
+  try {
+    const guild = client.guilds.cache.get(process.env.GUILD_ID);
+    client.guildInvites = await handler.fetchGuildInvites(guild);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-// Whenever a new invite is created, we refresh our cache
 client.on('inviteCreate', async (invite) => {
-  const server = await invite.guild.fetch();
-
-  client.guildInvites = await handler.fetchGuildInvites(invite.guild);
-
-  console.log(`Invite created for server ${server.name} (${server.id}) with URL: ${invite.url}`);
+  try {
+    const server = await invite.guild.fetch();
+    client.guildInvites = await handler.fetchGuildInvites(invite.guild);
+    console.log(`Invite created for server ${server.name} (${server.id}) with URL: ${invite.url}`);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-// we retrieve the new Invites values and compare them with  their previous values.
 client.on('guildMemberAdd', async (member) => {
-  const userData = await handler.onNewUser(member, client);
-  console.log('New User: ', userData);
+  try {
+    await handler.onNewUser(member, client)
+      .then(async (user) => {
+        if (user) {
+          const {userId, userCode, userName, joinedAt} = user
+          console.log('New User: ', {userId: userId, code: userCode, userName: userName, time: joinedAt});
 
-  await sendEvent(userData.userId, userData.code);
+          const insertQuery = `INSERT INTO ${table} (userId, userCode, userName, timestamp) VALUES (?, ?, ?, ?)`;
+          db.query(insertQuery, [userId, userCode, userName, new Date()], (err, result) => {
+            if (err) console.log('insertQuery error: ', err);
+            console.log('New User saved successfully into the database!');
+          });
+
+          await sendEvent(userId, userCode);
+        } else {
+          const userCode = ' ';
+          const userId = `${member.user.id}`;
+          const userName = `${member.user.username}`;
+          console.log(`${userName} with the ID: ${userId} has Joined without using a custom link`)
+    
+          const insertQuery = `INSERT INTO ${table} (userId, userCode, userName, timestamp) VALUES (?, ?, ?, ?)`;
+          db.query(insertQuery, [userId, userCode, userName, new Date()], (err, result) => {
+            if (err) console.log('insertQuery error: ', err);
+            console.log('New User saved successfully into the database!');
+          });
+        }
+      });
+
+    client.guildInvites = await handler.fetchGuildInvites(member.guild);
+  } catch(err) {
+    console.log(err);
+  }
 });
 
-// Log our bot to discord API.
 client.login(process.env.DISCORD_BOT_TOKEN);
